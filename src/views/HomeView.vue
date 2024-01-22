569531c8cab6bfde89a0e9e038f63e71b1b02c19
serpents22 <ivandarmawan67@gmail.com>
2024-01-22 21:30:15 +0700

    feat: change language to italian

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@ import { ref } from 'vue'
 <template>
   <div class="bg-[#353535] xl:bg-[url('@/assets/login-bg.png')] w-full h-full bg-cover absolute z-0 flex flex-col gap-4 lg:gap-20 p-[20px]">
     <div class="mx-auto text-white pt-10 lg:pt-20">
-      <h1 class="text-2xl">Welcome to</h1>
-      <h1 class="text-[40px] font-semibold">Idrobit Portal</h1>
+      <h1 class="text-2xl">Benvenuti nei</h1>
+      <h1 class="text-[40px] font-semibold">Portali di Idrobit</h1>
     </div>
     <div class="flex flex-col items-center mx-auto gap-4 lg:gap-10">
       <div class="flex flex-col lg:flex-row gap-4 lg:gap-10">
@@ -36,6 +36,6 @@ p {
 }
 
 .card {
-  @apply bg-white rounded-lg shadow-lg items-center justify-center flex p-6 w-60
+  @apply bg-white rounded-lg shadow-lg items-center justify-center flex p-6 w-60 hover:scale-105 transition-transform duration-200
 }
 </style>
